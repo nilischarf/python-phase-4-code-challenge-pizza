e5bd674f72f0fe0f6587dcb052bfdd8e3935fa4f
Nili Scharf <nilischarf@gmail.com>
2025-05-26 12:30:56 +0300

    pizza

--- server/app.py
+++ server/app.py
@@ -19,11 +19,64 @@ db.init_app(app)
 
 api = Api(app)
 
-
 @app.route("/")
 def index():
     return "<h1>Code challenge</h1>"
 
+class Restaurants(Resource):
+    def get(self):
+        restaurant_list = [r.to_dict() for r in Restaurant.query.all()]
+        response = make_response(restaurant_list, 200)
+        return response
+
+api.add_resource(Restaurants, '/restaurants')
+
+class RestaurantByID(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        restaurant_dict = restaurant.to_dict(['address', 'id', 'name', 'restaurant_pizzas'])
+        response = make_response(restaurant_dict, 200)
+        return response
+
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        db.session.delete(restaurant)
+        db.session.commit()
+        response = make_response('', 204)
+        return response
+
+api.add_resource(RestaurantByID, '/restaurants/<int:id>')
+
+class Pizzas(Resource):
+    def get(self):
+        pizza_list = [p.to_dict() for p in Pizza.query.all()]
+        response = make_response(pizza_list, 200,)
+        return response
+
+api.add_resource(Pizzas, '/pizzas')
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            price = data.get('price')
+            pizza_id = data.get('pizza_id')
+            restaurant_id = data.get('restaurant_id')
+
+            new_restaurant_pizza= RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+            db.session.add(new_restaurant_pizza)
+            db.session.commit()
+            response_dict = new_restaurant_pizza.to_dict()
+            response = make_response(response_dict, 201)
+            return response
+        except (ValueError, TypeError) as e:
+            return {"errors": ["validation errors"]}, 400
+
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
